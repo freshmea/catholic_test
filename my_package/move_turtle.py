24189561370d4067d707c2a992b8f3c7fc56d555
freshmea <freshmea@naver.com>
2023-02-16 10:53:48 +0900

    aa

--- my_package/move_turtle.py
+++ my_package/move_turtle.py
@@ -2,14 +2,19 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from rclpy.qos import QoSProfile
+from turtlesim.msg import Pose
 
 class M_turtle(Node):
   def __init__(self):
     super().__init__('move_turtle')
     self.qos = QoSProfile(depth = 10)
     self.pub = self.create_publisher(Twist, 'turtle1/cmd_vel', self.qos)
+    self.sub = self.create_subscription(Pose, '/turtle1/pose', self.subpose, self.qos)
     self.create_timer(0.1, self.pubmessage)
     self.vel = 0.0
+    self.x = 0.0
+    self.y = 0.0
+    self.theta = 0.0
 
   def pubmessage(self):
     msg = Twist()
@@ -20,10 +25,15 @@ class M_turtle(Node):
     msg.angular.y = 0.0
     msg.angular.z = 2.5
     self.pub.publish(msg)
-    self.get_logger().info(f'Seding message: [{msg}]')
+    # self.get_logger().info(f'Seding message: [{msg}]')
     self.vel += 0.04
     if self.vel > 3.0:
       self.vel = 0.0
+
+  def subpose(self, msg):
+    self.x = msg.x
+    self.y = msg.y
+    self.theta = msg.theta
 
 def main():
   rclpy.init()
