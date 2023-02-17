577caace2663040a45ef80f757d9357619f700cd
freshmea <freshmea@naver.com>
2023-02-17 13:47:26 +0900

    aa

--- my_package/move_arm.py
+++ my_package/move_arm.py
@@ -1,39 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright (c) 2011, Willow Garage, Inc.
-# All rights reserved.
-#
-# Software License Agreement (BSD License 2.0)
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of {copyright_holder} nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Will Son
-
 from math import exp
 import os
 import rclpy
@@ -64,29 +28,6 @@ task_position_delta = 0.01  # meter
 joint_angle_delta = 0.05  # radian
 path_time = 0.5  # second
 
-usage = """
-Control Your OpenManipulator!
----------------------------
-Task Space Control:
-         (Forward, X+)
-              W                   Q (Upward, Z+)
-(Left, Y+) A     D (Right, Y-)    Z (Downward, Z-)
-              X
-        (Backward, X-)
-
-Joint Space Control:
-- Joint1 : Increase (Y), Decrease (H)
-- Joint2 : Increase (U), Decrease (J)
-- Joint3 : Increase (I), Decrease (K)
-- Joint4 : Increase (O), Decrease (L)
-- Gripper: Increase (F), Decrease (G) | Fully Open (V), Fully Close (B)
-
-INIT : (1)
-HOME : (2)
-
-CTRL-C to quit
-"""
-
 e = """
 Communications Failed
 """
@@ -96,8 +37,7 @@ class TeleopKeyboard(Node):
 
     qos = QoSProfile(depth=10)
     settings = None
-    if os.name != 'nt':
-        settings = termios.tcgetattr(sys.stdin)
+    settings = termios.tcgetattr(sys.stdin)
 
     def __init__(self):
         super().__init__('teleop_keyboard')
@@ -210,7 +150,6 @@ def get_key(settings):
     return key
 
 def print_present_values():
-    print(usage)
     print('Joint Angle(Rad): [{:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}]'.format(
         present_joint_angle[0],
         present_joint_angle[1],
@@ -229,8 +168,7 @@ def print_present_values():
 
 def main():
     settings = None
-    if os.name != 'nt':
-        settings = termios.tcgetattr(sys.stdin)
+    settings = termios.tcgetattr(sys.stdin)
 
     try:
         rclpy.init()
@@ -335,8 +273,7 @@ def main():
         print(e)
 
     finally:
-        if os.name != 'nt':
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
         teleop_keyboard.destroy_node()
         rclpy.shutdown()
 
